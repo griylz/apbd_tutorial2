ed79cd83049f9a750a6ea5bc4f3e962f875df0da
s25365 <s25365@pjwstk.edu.pl>
2024-03-08 18:48:43 +0100

    feature-average

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -5,3 +5,18 @@ Console.WriteLine("Modification 3!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1!");
 Console.WriteLine("Modification 2!");
+
+
+static double CountAvg(int[] arr)
+{
+    double res = 0;
+    foreach (int i in arr)
+    {
+        res += i;
+    }
+
+    return res / arr.Length;
+}
+
+int[] numbers = {1,3,4,5,6};
+Console.WriteLine(CountAvg(numbers));
