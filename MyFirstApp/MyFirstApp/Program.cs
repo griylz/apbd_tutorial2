73f88bad18462f28fe54ad1acd71c1a8cad1f5c3
s25365 <s25365@pjwstk.edu.pl>
2024-03-08 18:29:31 +0100

    Modification 3

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+Console.WriteLine("Modification 3!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1!");
 Console.WriteLine("Modification 2!");
